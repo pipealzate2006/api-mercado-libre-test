a7ba4d2892e2218d8745c663d9cf4c3d67b52f5d
Felipe Alzate Gil <felipegil444@gmail.com>
2024-08-13 15:39:43 -0500

    new cors

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -1,6 +1,6 @@
 import cors from "cors";
 
-export const ACCEPTED_ORIGINS = ["http://localhost:7020"];
+export const ACCEPTED_ORIGINS = ["http://localhost:7020", "https://api-mercado-libre-test.onrender.com/"];
 
 export const corsMiddleWares = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) =>
   cors({
