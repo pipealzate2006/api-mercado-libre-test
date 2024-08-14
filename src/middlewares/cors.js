7a97022f2f4609867aea07f641a92af4563920a2
Felipe Alzate Gil <felipegil444@gmail.com>
2024-08-14 08:56:20 -0500

    adding a new domain for the cors

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -1,6 +1,6 @@
 import cors from "cors";
 
-export const ACCEPTED_ORIGINS = ["http://localhost:5173"];
+export const ACCEPTED_ORIGINS = ["http://localhost:5173", "https://moonlit-fenglisu-e713ef.netlify.app"];
 
 export const corsMiddleWares = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) =>
   cors({
